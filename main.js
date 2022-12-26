206754ce7e969021e58ea293bdbf399d9aeca1ab
dilan-sachintha97 <dil97an@gmail.com>
2022-12-26 11:32:28 +0530

    loaded data to table

--- main.js
+++ main.js
@@ -18,7 +18,25 @@ const getAllPosts=()=>{
     })
 }*/
 
+
+/*
+
+// How to use Ajax
 let posts = []
+const getAllPosts=()=>{
+   $.ajax({
+       url:'https://jsonplaceholder.typicode.com/posts',
+       type:'GET',
+       dataType:'json',
+       success:(response)=>{
+           console.log(response)
+       }
+   })
+}*/
+
+
+let posts = []
+
 async function getAllPosts(){
     const res = await fetch('https://jsonplaceholder.typicode.com/posts',{
         method:'GET',
@@ -26,7 +44,28 @@ async function getAllPosts(){
             'Content-Type': 'application/json'  // data ana type aka
         })
     })
-
     posts = await res.json();
+
+    let dataSet = ''
+    posts.forEach(element=>{
+       dataSet += `<tr>
+                         <td>
+                            ${element.userId}
+                         </td>
+                         <td>
+                            ${element.id}
+                         </td>
+                         <td>
+                            ${element.title}
+                         </td>
+                         <td>
+                            ${element.body}
+                         </td>
+                         <td>
+                            <button class="btn btn-danger">Delete</button>
+                         </td>
+                   </tr>`
+    })
+    $('tbody').html(dataSet)   // use jqery
     console.log(posts)
 }
